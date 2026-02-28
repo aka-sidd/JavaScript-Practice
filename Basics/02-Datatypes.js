3c091b48c884b9ab709c56a5c8087908043067d2
Siddharth Pandey <pandeysiddhartha1515@gmail.com>
2026-02-28 19:19:40 +0530

    Basics of JS and Conversion of DataTypes

---
+++ Basics/02-Datatypes.js
@@ -0,0 +1,11 @@
+console.log(3+3)
+console.log("Siddharth")
+// number => 2 to power 53
+// bigint => more than 2 to power 53
+// string => "Siddharth"
+// boolean => true or false
+// null(StandAlone value) => empty value
+// undefined => value not assigned
+// symbol => unique identifier
+console.log(typeof "Siddharth")
+console.log(typeof 3)
---
+++ Basics/03-DatatyeConversion.js
@@ -0,0 +1,5 @@
+let score = 33;
+console.log(score, typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(valueInNumber, typeof valueInNumber)
