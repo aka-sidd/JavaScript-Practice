e3cf6aa4d48e42dfdb82ac89202cd753b62ac236
Siddharth Pandey <pandeysiddhartha1515@gmail.com>
2026-03-05 23:24:43 +0530

    Add Javascript datatype conversion and comparison basics

--- Basics/03-DatatyeConversion.js
+++
@@ -1,5 +0,0 @@
-let score = 33;
-console.log(score, typeof score)
-console.log(typeof(score))
-let valueInNumber = Number(score)
-console.log(valueInNumber, typeof valueInNumber)
---
+++ Basics/03-DatatypeConversion.js
@@ -0,0 +1,22 @@
+let score = false;
+//console.log(score, typeof score)
+//console.log(typeof(score))
+//let valueInNumber = Number(score)
+//console.log(valueInNumber, typeof valueInNumber)
+//console.log(valueInNumber)
+//let isLoggedIn = 1;
+let isLoggedIn = "Siddharth"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+let StringNumber = 33;
+let stringNumber = String(StringNumber)
+console.log(stringNumber)   
+console.log(typeof stringNumber)
+
+//1 -> true
+//0 -> false
+//"" -> false
+// "Siddharth" -> true
+//null -> false
+//undefined -> false
+
